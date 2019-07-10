d44f9c4c87fe33d13e58c60d864d36fb147091d2
Mark O'Brien <mobrienssoext@outlook.com>
2019-07-10 04:52:08 +0000

    working initial query and connect to database 	modified:   pg_tool.py

--- pg_tool.py
+++ pg_tool.py
@@ -4,3 +4,20 @@ import psycopg2
 
 db = psycopg2.connect("dbname=news")
 
+cursor = db.cursor()
+
+select_table_query = '''select * from authors;'''
+
+""" We can retrieve query result using cursor methods such as fetchone(), fetchmany(), fetcthall()."""
+
+
+cursor.execute(select_table_query)
+my_value = cursor.fetchall()
+print(my_value)
+
+
+
+cursor.close()
+db.close()
+
+
